6004341801c8f1f623d4ed0cdefc80662d293290
Pannaga Rao <pannaga.murthy@gmail.com>
2020-05-03 01:38:49 -0400

    Wall and key creation

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Vyuha1
-2D boardgame layout design using C++ and SFML 2.5.1
+2D game layout design using C++ and SFML 2.5.1
 
-
-## Player Sprite Creation
+## Sprite Creation
 I have used Windows 10 Paint 3D to create character animations for the game.
 
-Player is the main character modeled to mimic movement in all four directions when left/right/up/down arrow key is pressed. Player also displays weapon in all four directions when A/W/S/D key is pressed. 
+Player is the main character modelled to mimic movement in all four directions when left/right/up/down arrow key is pressed. Player also displays weapon in all four directions when A/W/S/D key is pressed.
 
 There are some replicas in the sprite sheet which will act as place holders for additional player animations.
 
 ![](pirate_sprite_sheet.png)
 
-Treasure is a static character placed at the end point of game for the player to reach.
+Key is a static element placed at the end point of game for the player to reach.
 
-![](treasurefront.png)
+![](key.png)
+
+Wall is a static element, that the player has to navigate through to reach the Key.
+
+![](wall1.png)
+
+## Game layout so far
+![](screenshot1.png)
--- vyuha1.cpp
+++ vyuha1.cpp
@@ -1,36 +1,48 @@
 #include<SFML/Graphics.hpp>
+#include<iostream>
+#include<list>
 using namespace std;
 using namespace sf;
 
-//Window size
-unsigned int xWin = 1000, yWin = 600;
+//Window size set using only modes supporting fullscreeen
+unsigned int xWin = 1280, yWin = 600;
 unsigned int FRLimit = 60;
 string playerAnim = "pirate_sprite_sheet.png";
-string treasureAnim = "treasurefront.png";
+string wallAnim = "wall1.png";
+string keyAnim = "key.png";
+string iconImage = "pirateicon64.png";
 
 class Character {
 public:
-	sf::Sprite sprite;
-	sf::Texture texture;
+	Sprite sprite;
+	Texture texture;
 	int left, top, width, height;
-	Character():left(0), top(0), width(0), height(0) {}
-	Character(sf::Texture& t, string filename, int x=0, int y=0, int w=0, int h=0):left(x), top(y), width(w), height(h)
+	int frameCount;
+	Character():left(0), top(0), width(0), height(0), frameCount(1) {}
+	Character(Texture& t, string filename, int x=0, int y=0, int w=0, int h=0, int f=1):left(x), top(y), width(w), height(h), frameCount(f)
 	{
 		texture = t;
 		texture.loadFromFile(filename);
 		texture.setSmooth(true);
-		changePosture(x, y, w, h);
+		changePosture(x, y, w, h, f);
 	}
-	void changePosture(int x, int y, int w, int h)
+	void changePosture(int x, int y, int w, int h, int f)
 	{
+		std::vector<IntRect> animFrames;
+		int i = 0;
+		while (i < f)
+		{
+			animFrames.push_back(IntRect(x + (w * i), y, w, h));
+			i++;
+		}
 		sprite.setTexture(texture);
-		sprite.setTextureRect(IntRect(x, y, w, h));
+		sprite.setTextureRect(animFrames[0]);
 	}
 	void checkBoundaryCollision(sf::FloatRect bounds)
 	{
 		//Collision detection - Player vs Window boundary line(2D obj vs 1D obj)
 		//Adapt player position to be within window boundary
-		sf::Vector2f position = sprite.getPosition();
+		Vector2f position = sprite.getPosition();
 		position.x = std::max(position.x, bounds.left);
 		position.y = std::max(position.y, bounds.top);
 		sprite.setPosition(position);
@@ -38,81 +50,115 @@ public:
 		position.y = std::min(position.y, bounds.height - height);
 		sprite.setPosition(position);
 	}
+	//Add a method to change frames later
 };
+
 int main()
 {
 	//Window
-	sf::RenderWindow window(sf::VideoMode(xWin, yWin), "Vyuha-Level 1", sf::Style::Close);
+	RenderWindow window(VideoMode(xWin, yWin), "Vyuha-Level 1", Style::Default);
 	window.setFramerateLimit(FRLimit);
 	//Get window boundary, getSize returns only float so convert the rest
-	sf::FloatRect windowBounds(sf::Vector2f(0.f, 0.f), window.getDefaultView().getSize());
+	FloatRect windowBounds(Vector2f(0.f, 0.f), window.getDefaultView().getSize());
+	//Set Icon
+	Image icon;
+	icon.loadFromFile(iconImage);
+	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+	
 	//Texture & Sprite
-	sf::Texture pTexture, tTexture;
+	Texture pTexture, wTexture, kTexture;
 	Character player(pTexture, playerAnim, 0, 0, 60, 89);
-	player.sprite.setPosition(float(xWin / 2 - 60 / 2), float(yWin - 89));
-	Character treasure(tTexture, treasureAnim, 0, 0, 198, 150);
-	treasure.sprite.setPosition(float(xWin / 2 -198/2), float(yWin / 2 - 150 / 2));
+	player.sprite.setPosition(0.f, float(yWin - 89));
+
+	list<Character>walls;
+	/*wallCoverage matrix columns : width, height, xPos, level*/
+	vector<vector<float>>wallCoverage{ {616.f,85.f,0.f,1.f}, {616.f,85.f,700.f,1.f}, \
+									   {308.f,85.f,0.f,2.f}, {616.f,85.f,400.f,2.f}, {200.f,85.f,1100.f,2.f}, \
+									   {616.f,85.f,0.f,3.f}, {480.f,85.f,610.f,3.f} };
+	for (unsigned int i = 0; i < wallCoverage.size(); i++)
+	{
+		Character block(wTexture, wallAnim, 0, 0, int(wallCoverage[i][0]), int(wallCoverage[i][1]));
+		block.sprite.setPosition(float(wallCoverage[i][2]), \
+								 float(yWin - (85+89)*wallCoverage[i][3] - (15.f*wallCoverage[i][3])));
+		walls.push_back(block);
+	}
+	
+	Character key(kTexture, keyAnim, 0, 0, 105, 172);
+	key.sprite.setPosition(float(xWin) - 105.f, 0.f);
+
 	//Event
-	sf::Event event;
+	Event event;
 	//Move
 	float moveUnitMin, moveUnitMax;
-	moveUnitMin = -8, moveUnitMax = 8;
+	moveUnitMin = -5.f, moveUnitMax = 5.f;
 
 	//Game loop
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
-			if (event.type == sf::Event::KeyPressed)
+			if (event.type == Event::KeyPressed)
 			{
 				//Movement controls left, right, up and down arrow keys
-				if (event.key.code == sf::Keyboard::Right)
+				if (event.key.code == Keyboard::Right)
 				{
 					player.sprite.move(moveUnitMax, 0);
-					player.changePosture(60, 0, 60, 89);
+					player.changePosture(60, 0, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::Left)
+				if (event.key.code == Keyboard::Left)
 				{
 					player.sprite.move(moveUnitMin, 0);
-					player.changePosture(120, 0, 60, 89);
+					player.changePosture(120, 0, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::Up)
+				if (event.key.code == Keyboard::Up)
 				{
 					player.sprite.move(0, moveUnitMin);
-					player.changePosture(180, 0, 60, 89);
+					player.changePosture(180, 0, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::Down)
+				if (event.key.code == Keyboard::Down)
 				{
 					player.sprite.move(0, moveUnitMax);
-					player.changePosture(0, 0, 60, 89);
+					player.changePosture(0, 0, 60, 89, 1);
 				}
 				//Weapon display controls A, D, W and S letter keys
-				if (event.key.code == sf::Keyboard::A)
+				if (event.key.code == Keyboard::A)
 				{
-					player.changePosture(120, 90, 60, 89);
+					player.changePosture(120, 90, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::D)
+				if (event.key.code == Keyboard::D)
 				{
-					player.changePosture(60, 90, 60, 89);
+					player.changePosture(60, 90, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::W)
+				if (event.key.code == Keyboard::W)
 				{
-					player.changePosture(180, 90, 60, 89);
+					player.changePosture(180, 90, 60, 89, 1);
 				}
-				if (event.key.code == sf::Keyboard::S)
+				if (event.key.code == Keyboard::S)
 				{
-					player.changePosture(0, 90, 60, 89);
+					player.changePosture(0, 90, 60, 89, 1);
 				}
 			}
 		}
+		//Update
+
 		//Collision detection
 		player.checkBoundaryCollision(windowBounds);
+		for (auto wall : walls)
+			if (player.sprite.getGlobalBounds().intersects(wall.sprite.getGlobalBounds()))
+			{
+				cout <<"touched" << player.sprite.getGlobalBounds().left<<","<< player.sprite.getGlobalBounds().top\
+				 <<" "<< player.sprite.getGlobalBounds().width<<","<< player.sprite.getGlobalBounds().height<<endl;
+				//TODO
+			}
 		//Display all
-		window.clear(sf::Color::White);
+		window.clear(Color(30,30,40,255));//Night shade(30,30,40)
+		//window.clear(Color(20, 20, 20, 255));//Dark Grey(20,20,20)
 		window.draw(player.sprite);
-		window.draw(treasure.sprite);
+		for (auto wall: walls)
+			window.draw(wall.sprite);
+		window.draw(key.sprite);
 		window.display();
 	}
 	return 0;
