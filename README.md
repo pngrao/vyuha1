524fe3fe041fe45887ac99c29058d69b15b017d2
Pannaga Rao <pannaga.murthy@gmail.com>
2020-05-04 02:23:49 -0400

    Four direction collision detection

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 | Application icon | sf::Image and sf::RenderWindow |
 | Player, Key and Wall animation | sf::Texture, sf::Sprite and Character class |
 | Player movements | Character class member functions, sf::Sprite and sf::Event's KeyPressed event check |
-| Boundary collision detection | sf::Sprite's getPosition() and sf::RenderWindow's getGlobalBounds() |
+| Boundary collision detection | sf::Sprite's getPosition() and sf::RenderWindow's getGlobalBounds() are used in Character class's checkBoundaryCollision() |
+| Player vs Wall/Key collision | sf::Sprite's getGlobalBounds() is used to check intersects() and based on 4 direction collision condition, player's position is reset in Character class's checkObjectCollision() |
 
 ## Sprite Creation
 I have used Windows 10 Paint 3D to create character animations for the game.
@@ -27,5 +28,9 @@ Wall is a static element, that the player has to navigate through to reach the K
 
 ![](wall1.png)
 
+Game icon in title bar and thumbnail:
+
+![](pirateicon64.png)
+
 ## Game layout so far
-![](screenshot_may3.png)
+![](screenshot_may4.png)
--- vyuha1.cpp
+++ vyuha1.cpp
@@ -26,6 +26,7 @@ public:
 		texture.setSmooth(true);
 		changePosture(x, y, w, h, f);
 	}
+	//TODO overload changePosture to accept vector<vector<int>>
 	void changePosture(int x, int y, int w, int h, int f)
 	{
 		std::vector<IntRect> animFrames;
@@ -42,6 +43,7 @@ public:
 	{
 		//Collision detection - Player vs Window boundary line(2D obj vs 1D obj)
 		//Adapt player position to be within window boundary
+		//TODO change sprite dimension- right turn showing gap
 		Vector2f position = sprite.getPosition();
 		position.x = std::max(position.x, bounds.left);
 		position.y = std::max(position.y, bounds.top);
@@ -49,6 +51,48 @@ public:
 		position.x = std::min(position.x, bounds.width - width);
 		position.y = std::min(position.y, bounds.height - height);
 		sprite.setPosition(position);
+	}
+	void checkObjectCollision(Character* a, Character* b, Vector2f &v)
+	{
+		FloatRect aBounds = a->sprite.getGlobalBounds();
+		FloatRect bBounds = b->sprite.getGlobalBounds();
+		aBounds.left += v.x;
+		aBounds.top += v.y;
+		if (aBounds.intersects(bBounds))
+		{
+			//right wall
+			if (aBounds.left < bBounds.left &&
+				aBounds.left + aBounds.width < bBounds.left + bBounds.width &&
+				aBounds.top < bBounds.top + bBounds.height &&
+				aBounds.top + aBounds.height > bBounds.top)
+			{
+				a->sprite.setPosition(bBounds.left - aBounds.width, aBounds.top);
+			}
+			//left wall
+			else if (aBounds.left > bBounds.left &&
+				aBounds.left + aBounds.width > bBounds.left + bBounds.width &&
+				aBounds.top < bBounds.top + bBounds.height &&
+				aBounds.top + aBounds.height > bBounds.top)
+			{
+				a->sprite.setPosition(bBounds.left + bBounds.width, aBounds.top);
+			}
+			//bottom wall
+			else if (aBounds.top < bBounds.top &&
+				aBounds.top + aBounds.height < bBounds.top + bBounds.height &&
+				aBounds.left < bBounds.left + bBounds.width &&
+				aBounds.left + aBounds.width > bBounds.left)
+			{
+				a->sprite.setPosition(aBounds.left, bBounds.top - aBounds.height);
+			}
+			//top wall
+			else if (aBounds.top > bBounds.top &&
+				aBounds.top + aBounds.height > bBounds.top + bBounds.height &&
+				aBounds.left < bBounds.left + bBounds.width &&
+				aBounds.left + aBounds.width > bBounds.left)
+			{
+				a->sprite.setPosition(aBounds.left, bBounds.top + aBounds.height);
+			}
+		}
 	}
 	//Add a method to change frames later
 };
@@ -67,8 +111,15 @@ int main()
 	
 	//Texture & Sprite
 	Texture pTexture, wTexture, kTexture;
+
 	Character player(pTexture, playerAnim, 0, 0, 60, 89);
 	player.sprite.setPosition(0.f, float(yWin - 89));
+	vector<vector<int>>playerPostures{ /*{60, 0, 60, 89, 1}, {120, 0, 60, 89, 1},\ */ //right and left changed
+									   {60, 0, 45, 89, 1}, {130, 0, 45, 89, 1},\
+									   {180, 0, 60, 89, 1}, {0, 0, 60, 89, 1},\
+									   {120, 90, 60, 89, 1}, {60, 90, 60, 89, 1},\
+									   {180, 90, 60, 89, 1}, { 0, 90, 60, 89, 1} };
+	int posture = 3;
 
 	list<Character>walls;
 	/*wallCoverage matrix columns : width, height, xPos, level*/
@@ -90,7 +141,7 @@ int main()
 	Event event;
 	//Move
 	float moveUnitMin, moveUnitMax;
-	moveUnitMin = -5.f, moveUnitMax = 5.f;
+	moveUnitMin = -3.f, moveUnitMax = 3.f;
 
 	//Game loop
 	while (window.isOpen())
@@ -99,59 +150,63 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
-			if (event.type == Event::KeyPressed)
-			{
-				//Movement controls left, right, up and down arrow keys
-				if (event.key.code == Keyboard::Right)
-				{
-					player.sprite.move(moveUnitMax, 0);
-					player.changePosture(60, 0, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::Left)
-				{
-					player.sprite.move(moveUnitMin, 0);
-					player.changePosture(120, 0, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::Up)
-				{
-					player.sprite.move(0, moveUnitMin);
-					player.changePosture(180, 0, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::Down)
-				{
-					player.sprite.move(0, moveUnitMax);
-					player.changePosture(0, 0, 60, 89, 1);
-				}
-				//Weapon display controls A, D, W and S letter keys
-				if (event.key.code == Keyboard::A)
-				{
-					player.changePosture(120, 90, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::D)
-				{
-					player.changePosture(60, 90, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::W)
-				{
-					player.changePosture(180, 90, 60, 89, 1);
-				}
-				if (event.key.code == Keyboard::S)
-				{
-					player.changePosture(0, 90, 60, 89, 1);
-				}
-			}
 		}
-		//Update
+		Vector2f velocity(0.f, 0.f);
+		//Movement controls left, right, up and down arrow keys
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			velocity.x = moveUnitMax;
+			posture = 0;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			velocity.x = moveUnitMin;
+			posture = 1;
+			
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			velocity.y = moveUnitMin;
+			posture = 2;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			velocity.y = moveUnitMax;
+			posture = 3;
+		}
+		//Weapon display controls A, D, W and S letter keys
+		if (Keyboard::isKeyPressed(Keyboard::A))
+		{
+			posture = 4;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D))
+		{
+			posture = 5;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::W))
+		{
+			posture = 6;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S))
+		{
+			posture = 7;
+		}
 
-		//Collision detection
+		//Update
+		player.sprite.move(velocity.x, velocity.y);
+		player.changePosture(playerPostures[posture][0], playerPostures[posture][1], \
+							 playerPostures[posture][2], playerPostures[posture][3], \
+							 playerPostures[posture][4]);
+		//Boundary collision detection
 		player.checkBoundaryCollision(windowBounds);
+		//Player to wall collision detection
 		for (auto wall : walls)
-			if (player.sprite.getGlobalBounds().intersects(wall.sprite.getGlobalBounds()))
-			{
-				cout <<"touched" << player.sprite.getGlobalBounds().left<<","<< player.sprite.getGlobalBounds().top\
-				 <<" "<< player.sprite.getGlobalBounds().width<<","<< player.sprite.getGlobalBounds().height<<endl;
-				//TODO
-			}
+		{
+			player.checkObjectCollision(&player, &wall, velocity);
+		}
+		//Player to key collision detection
+		player.checkObjectCollision(&player, &key, velocity);
+
 		//Display all
 		window.clear(Color(30,30,40,255));//Night shade(30,30,40)
 		//window.clear(Color(20, 20, 20, 255));//Dark Grey(20,20,20)
